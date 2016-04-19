c5e70e4f9476d086610cee112ceb55de08e85d0b
Darron Froese <darron@froese.org>
2016-04-18 19:19:17 -0600

    Let's raise the open files limit.

--- fpm-recipes/consul/recipe.rb
+++ fpm-recipes/consul/recipe.rb
@@ -2,7 +2,7 @@ class Consul < FPM::Cookery::Recipe
   name 'consul'
 
   version '0.6.4'
-  revision '1'
+  revision '2'
   description 'Consul Service Discovery Platform'
 
   homepage 'https://www.consul.io'
